60331ac2de7cbf23c720be716ca761cd4a1a2dfa
kale <jinkai.jk@alibaba-inc.com>
2023-06-10 23:32:13 +0800

    支持控制feedback显隐

--- src/Controller.ts
+++ src/Controller.ts
@@ -1,28 +1,37 @@
-import { ActionInfoType, ConversationDataType, SYS_ACTION_NAME, SysViewElementInfo } from '../Interface'
+import { ActionInfoType, ActionHandleResultType, SYS_ACTION_NAME, SysViewElementInfo, FeedbackInfoType } from '../Interface'
+import { gid } from '../index'
 
 const md5 = require('js-md5')
 
 class Controller {
 
-  public async handleAction({ action, expectation, values }: ActionInfoType): Promise<ConversationDataType> {
+  constructor() {
+    console.log('gid',gid)
+  }
+
+  public handleFeedback(feedbackInfo: FeedbackInfoType) {
+  }
+
+  public async handleAction({ action, expectation, values }: ActionInfoType): Promise<ActionHandleResultType> {
     console.log('handle action:', action, expectation, values)
 
-    if (action === SYS_ACTION_NAME.INITIALIZATION || action === 'RE_INPUT') {
-      return {
-        conversationId: 'id:' + Math.random(),
-        viewElementInfos: [new SysViewElementInfo.ChatBox({ placeholder: 'please input some text' }, 'exp')],
-      }
+    switch (action) {
+      case SYS_ACTION_NAME.INITIALIZATION:
+      case 'RE_INPUT':
+        return {
+          sessionUUId: 'id:' + Math.random(),
+          viewElementInfos: [new SysViewElementInfo.ChatBox({ placeholder: 'please input some text' }, 'exp')],
+          canFeedback: false,
+        }
+      case SYS_ACTION_NAME.CHAT_BOX_SUBMIT:
+        return {
+          sessionUUId: 'id:' + Math.random(),
+          viewElementInfos: [{ viewType: 'md5Text', data: { md5Str: md5(values.text) } }],
+          suggestActions: [{ label: '再次输入', actionInfo: { action: 'RE_INPUT' } }],
+        }
+      default:
+        return { sessionUUId: '', viewElementInfos: [] }
     }
-
-    if (action === SYS_ACTION_NAME.CHAT_BOX_SUBMIT) {
-      return {
-        conversationId: 'id:' + Math.random(),
-        viewElementInfos: [{ viewType: 'md5Text', data: { md5Str: md5(values.text) } }],
-        suggestActions: [{ label: '再次输入', actionInfo: { action: 'RE_INPUT' } }],
-      }
-    }
-
-    return { conversationId: '', viewElementInfos: [] } // default
   }
 }
 
