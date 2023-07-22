11f7ec415a832c643ddc1d956c7d1d5a4f902886
kale <jinkai.jk@alibaba-inc.com>
2023-07-23 00:30:10 +0800

    将道具渲染放在道具的getView方法中，避免报错

--- src/View.tsx
+++ src/View.tsx
@@ -1,6 +1,5 @@
 import { IViewElementProps } from '../Interface'
 
-export default (
-  { viewType, data, expectation, onSendAction: sendAction }: IViewElementProps) => {
+export default ({ viewType, data, expectation, onSendAction: sendAction }: IViewElementProps) => {
   return viewType === 'md5Text' ? <span>{data.md5Str}</span> : <div />
 }
